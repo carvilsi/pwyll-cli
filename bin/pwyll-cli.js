ac42d98313519ce93c0c6374114f16f9252a0eac
carvilsi <carvilsi.mail.list@gmail.com>
2024-02-27 13:45:50 +0100

    adding aliases (long names) to commands

--- bin/pwyll-cli.js
+++ bin/pwyll-cli.js
@@ -1,10 +1,9 @@
 #! /usr/bin/env node
 
-'use strict';
 
 import { Command } from 'commander';
-import { signUpPrompt } from './../src/signUp.js'; 
-import { add } from './../src/addSnippet.js'; 
+import { signUpPrompt } from './../src/signUp.js';
+import { add } from './../src/addSnippet.js';
 import { search } from './../src/searchSnippet.js';
 import fs from 'fs';
 import * as url from 'url';
@@ -16,57 +15,62 @@ const pckg = JSON.parse(fs.readFileSync(path.join(__dirname, './../package.json'
 const program = new Command();
 
 program
-  .name(pckg.name)
-  .description(`${pckg.description}
+    .name(pckg.name)
+    .description(`${pckg.description}
                   ┓┓  ┓•
            ┏┓┓┏┏┓┏┃┃ ┏┃┓
            ┣┛┗┻┛┗┫┗┗━┗┗┗
            ┛     ┛      
           <3 by carvilsi`)
-  .version(pckg.version)
-  .usage('[options] command')
-  .command('signup')
-  .description('do the sign up, creating a new user and dealing with configuration')
-  .action(() => {
-    signUpPrompt();
-  });
-  
-program  
-  .command('s')
-  .description('searches snippets for the current signed up user and copies the ' + 
+    .version(pckg.version)
+    .usage('[options] command')
+    .command('signup')
+    .description('do the sign up, creating a new user and dealing with configuration')
+    .action(() => {
+        signUpPrompt();
+    });
+
+program
+    .command('s')
+    .alias('search')
+    .description('searches snippets for the current signed up user and copies the ' +
                'selected one to the clipboard')
-  .action(() => {
-    search();
-  });
-   
-program  
-  .command('sa')
-  .description('searches snippets from any user and copies the ' + 
+    .action(() => {
+        search();
+    });
+
+program
+    .command('sa')
+    .alias('search-all')
+    .description('searches snippets from any user and copies the ' +
                'selected one to the clipboard')
-  .action(() => {
-    search({ searchAll: true });
-  });
-  
-program  
-  .command('n')
-  .description('creates new snippet for the current signed up user')
-  .action(() => {
-    add();
-  });
-  
-program  
-  .command('u')
-  .description('updates a snippet for the current signed up user')
-  .action(() => {
-    search({ update: true });
-  });
-  
-program  
-  .command('d')
-  .description('deletes a snippet for the current signed up user')
-  .action(() => {
-    search({ del: true });
-  });
+    .action(() => {
+        search({ searchAll: true });
+    });
+
+program
+    .command('n')
+    .alias('new')
+    .description('creates new snippet for the current signed up user')
+    .action(() => {
+        add();
+    });
+
+program
+    .command('u')
+    .alias('update')
+    .description('updates a snippet for the current signed up user')
+    .action(() => {
+        search({ update: true });
+    });
+
+program
+    .command('d')
+    .alias('delete')
+    .description('deletes a snippet for the current signed up user')
+    .action(() => {
+        search({ del: true });
+    });
 
 program.parse();
 
