fae92436ef967322322a5e4d2ad1906596f409d7
carvilsi <carvilsi.mail.list@gmail.com>
2024-03-03 10:31:10 +0100

    fwip

--- src/exportSnippets.js
+++ src/exportSnippets.js
@@ -10,7 +10,9 @@ export default async function exportsFromPwyll(file) {
     try {
         const config = configReader();
         await checkVersion(config);
+        console.log('In the name of lol');
         if (fs.existsSync(file)) {
+            console.log('WTH!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
             infoHandler(`The export file ${file} already exists`);
             throw new Error(`The export file ${file} already exists`);
         }
