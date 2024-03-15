c4f14d1be853b6a1f804826950818c2607f998d9
carvilsi <carvilsi.mail.list@gmail.com>
2024-03-15 11:06:28 +0100

    retireves current version of pwyll's server side to update ci cd docker composition

--- pre-release.sh
+++ pre-release.sh
@@ -8,12 +8,14 @@ line="# [v${VERSION}](https:\/\/github.com\/carvilsi\/${NAME}\/releases\/tag\/v$
 last_release_date=$(head -n 3 CHANGELOG.md | tail -1 | awk '{ print $3 }' | sed 's/(\|)//g')
 commit_message=$(git log --after=${last_release_date} --format='- %s' | grep -v 'Merge' |  sed '{:q;N;s/\n//g;t q}')
 
-#echo $last_release_date;
-#echo $commit_message;
+TEST_DOCKER_COMPOSITION_FILE=tests/docker-compose.yml
+# Requires jq
+current_vers_develop_server=$(curl -s https://raw.githubusercontent.com/carvilsi/pwyll/develop/package.json | jq '.["version"]' | sed 's/\"//g')
 
+echo $current_ver_develop_server
+sed -i "s/pwyll:[0-9]*.[0-9]*.[0-9]*/pwyll:${current_vers_develop_server}/g" $TEST_DOCKER_COMPOSITION_FILE
 # TODO: retrieve the current pwyll server and update the docker-compose for testing
 
-sed -i '2s/^/\nnewchangelogentry\n/' $CHNGLG
-# FIXME: this is failing
-sed -i "s/newchangelogentry/${line}\n\n- ${commit_message}/g" $CHNGLG
+#sed -i '2s/^/\nnewchangelogentry\n/' $CHNGLG
+#sed -i "s/newchangelogentry/${line}\n\n- ${commit_message}/g" $CHNGLG
 
